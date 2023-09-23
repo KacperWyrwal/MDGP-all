6aada3156b2462345329124b30875ecc2a26c37b
Kacper Wyrwal <wyrwal.kacper@gmail.com>
2023-09-23 18:42:13 +0100

    Added an exact GP for baseline

---
+++ mdgp/models/exact_gps.py
@@ -0,0 +1,19 @@
+from torch import Tensor 
+from geometric_kernels.spaces import Space
+
+
+import gpytorch
+from mdgp.kernels import GeometricMaternKernel
+
+class GeometricManifoldGP(gpytorch.models.ExactGP): 
+    """
+    The simplest possible GP model with a GeometricMaternKernel taking in a space parameter.
+
+    TODO: We might need to do some reshaping in the forward function depending on the BO algo. 
+    """
+    def __init__(self, train_x: Tensor, train_y: Tensor, space: Space, nu: float = 2.5, 
+                 **kwargs):
+        likelihood = gpytorch.likelihoods.GaussianLikelihood()
+        super().__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.covar_module = GeometricMaternKernel(space=space, nu=nu, **kwargs)

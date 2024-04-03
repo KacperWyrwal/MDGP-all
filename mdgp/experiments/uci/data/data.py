57e671d011a29340aefb9e34d51da1e8b594091b
Kacper Wyrwal <wyrwal.kacper@gmail.com>
2024-04-03 18:37:59 +0100

    updated UCI configs for new datasets

--- mdgp/experiments/uci/data/data.py
+++ mdgp/experiments/uci/data/data.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field 
-from mdgp.experiments.uci.data.datasets import *
+from mdgp.experiments.uci.data.datasets import Energy, Power, Kin8mn, Concrete, Yacht, UCIDataset
 
 
 @dataclass
@@ -14,5 +14,9 @@ class DataArguments:
             return Power()
         elif self.dataset_name == 'concrete':
             return Concrete()
+        elif self.dataset_name == 'yacht':
+            return Yacht()
+        elif self.dataset_name == 'energy':
+            return Energy()
         else:
             raise ValueError(f"Unknown dataset: {self.dataset_name}")
